25b283ed29f7cf4d2aec479aa5928c6afbe20220
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2025-12-20 09:14:36 -0500

    Another attempt to optimize Day 4

--- src/day4/day4.zig
+++ src/day4/day4.zig
@@ -1,88 +1,122 @@
 const std = @import("std");
 const util = @import("../util.zig");
 
-const Coord = struct { x: usize, y: usize };
+const Adjacent = struct {
+    coords: [8]Point,
+    len: usize,
+};
+
+const Point = struct {
+    x: i32,
+    y: i32,
+
+    fn getAdjacent(self: *Point) Adjacent {
+        var res: Adjacent = undefined;
+        res.len = 0;
+
+        var dy: i32 = -1;
+        while (dy <= 1) : (dy += 1) {
+            var dx: i32 = -1;
+            while (dx <= 1) : (dx += 1) {
+                if (dx == 0 and dy == 0) {
+                    continue;
+                }
+
+                res.coords[res.len] = Point{
+                    .x = self.x + dx,
+                    .y = self.y + dy,
+                };
+                res.len += 1;
+            }
+        }
+
+        return res;
+    }
+};
 
 pub fn run() !void {
     const gpa = std.heap.page_allocator;
 
-    var result = try util.readLinesMutable(gpa, "src/day4/input.txt");
+    var result = try util.readLines(gpa, "src/day4/input.txt");
     defer result.deinit(gpa);
 
     std.debug.print("  Day 4 - Part 1\n", .{});
 
-    var grid: Grid = .{
-        .grid = result.lines,
-        .width = result.lines[0].len,
-        .height = result.lines.len,
-    };
-    var count: usize = grid.tick();
+    var grid = try Grid.initFromLines(result.lines, gpa);
+    defer grid.deinit();
 
-    std.debug.print("    Accessible Rolls = {}\n", .{count});
+    const rollCount = grid.rolls.count();
+
+    var nextGrid = try Grid.initWithCapacity(@intCast(rollCount), gpa);
+    defer nextGrid.deinit();
+
+    var grid_ptr = &grid;
+    var nextGrid_ptr = &nextGrid;
+
+    grid_ptr.tick(nextGrid_ptr);
+
+    std.debug.print("    Rolls Removed = {}\n", .{rollCount - nextGrid_ptr.rolls.count()});
+
     std.debug.print("  Day 4 - Part 2\n", .{});
 
-    var evalCount: usize = count;
-    while (evalCount > 0) {
-        evalCount = grid.tick();
-        count += evalCount;
+    while (grid.rolls.count() != nextGrid.rolls.count()) {
+        const temp = nextGrid_ptr;
+        nextGrid_ptr = grid_ptr;
+        grid_ptr = temp;
+
+        grid_ptr.tick(nextGrid_ptr);
     }
 
-    std.debug.print("    Rolls Removed = {}\n", .{count});
+    std.debug.print("    Rolls Removed = {}\n", .{rollCount - nextGrid_ptr.rolls.count()});
 }
 
 const Grid = struct {
-    grid: [][]u8,
-    width: usize,
-    height: usize,
+    rolls: std.AutoHashMap(Point, void),
 
-    fn tick(self: *Grid) usize {
-        var count: usize = 0;
+    fn initFromLines(lines: []const []const u8, gpa: std.mem.Allocator) !Grid {
+        const w = lines[0].len;
+        const h = lines.len;
 
-        for (0..self.height) |y| {
-            const row = self.grid[y];
-            for (0..self.width) |x| {
-                if (row[x] != '@') {
-                    continue;
+        var rolls = std.AutoHashMap(Point, void).init(gpa);
+        try rolls.ensureTotalCapacity(@intCast(w * h));
+
+        for (0..lines.len) |y| {
+            const line = lines[y];
+            for (0..line.len) |x| {
+                if (line[x] == '@') {
+                    rolls.putAssumeCapacity(.{ .x = @intCast(x), .y = @intCast(y) }, {});
                 }
+            }
+        }
 
-                var adjacentRolls: i32 = 0;
+        return .{ .rolls = rolls };
+    }
 
-                var dy: i32 = -1;
-                while (dy <= 1) : (dy += 1) {
-                    var dx: i32 = -1;
-                    while (dx <= 1) : (dx += 1) {
-                        if (dx == 0 and dy == 0) continue;
+    fn initWithCapacity(capacity: u32, gpa: std.mem.Allocator) !Grid {
+        var rolls = std.AutoHashMap(Point, void).init(gpa);
+        try rolls.ensureTotalCapacity(capacity);
+        return .{ .rolls = rolls };
+    }
 
-                        const x2_i: i32 = @as(i32, @intCast(x)) + dx;
-                        const y2_i: i32 = @as(i32, @intCast(y)) + dy;
+    fn deinit(self: *Grid) void {
+        self.rolls.deinit();
+    }
 
-                        if (0 <= x2_i and x2_i < @as(i32, @intCast(self.width)) and
-                            0 <= y2_i and y2_i < @as(i32, @intCast(self.height)))
-                        {
-                            const x2: usize = @intCast(x2_i);
-                            const y2: usize = @intCast(y2_i);
-
-                            if (self.grid[y2][x2] == '@' or self.grid[y2][x2] == 'a') {
-                                adjacentRolls += 1;
-                            }
-                        }
+    fn tick(self: *Grid, next: *Grid) void {
+        next.rolls.clearRetainingCapacity();
+        var iter = self.rolls.keyIterator();
+        while (iter.next()) |c| {
+            const adjPoints = c.getAdjacent();
+            var adjacentRolls: i32 = 0;
+            for (0..adjPoints.len) |i| {
+                if (self.rolls.contains(adjPoints.coords[i])) {
+                    adjacentRolls += 1;
+                    if (adjacentRolls >= 4) {
+                        next.rolls.putAssumeCapacity(c.*, {});
+                        break;
                     }
                 }
-
-                if (adjacentRolls < 4) {
-                    self.grid[y][x] = 'a';
-                    count += 1;
-                }
             }
         }
-
-        for (0..self.height) |y| {
-            const row = self.grid[y];
-            for (0..self.width) |x| {
-                if (row[x] == 'a') row[x] = '.';
-            }
-        }
-
-        return count;
     }
 };
