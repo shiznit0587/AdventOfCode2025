38e38d7b8c5709c596432f537f81c6dcb8ecf527
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2025-12-20 10:26:01 -0500

    Finished Day 7

--- src/day7/day7.zig
+++ src/day7/day7.zig
@@ -4,9 +4,9 @@ const util = @import("../util.zig");
 pub fn run() !void {
     const gpa = std.heap.page_allocator;
 
-    var lines = try util.readLines(gpa, "src/day7/input.txt");
-    defer lines.deinit(gpa);
-    const width = lines.lines[0].len;
+    var rlr = try util.readLines(gpa, "src/day7/input.txt");
+    defer rlr.deinit(gpa);
+    const width = rlr.lines[0].len;
 
     std.debug.print("  Day 7 - Part 1\n", .{});
 
@@ -14,17 +14,16 @@ pub fn run() !void {
     defer beams.deinit(gpa);
 
     for (0..width) |x| {
-        beams.appendAssumeCapacity(if (lines.lines[0][x] == 'S') 1 else 0);
+        beams.appendAssumeCapacity(if (rlr.lines[0][x] == 'S') 1 else 0);
     }
 
     var splits: usize = 0;
     var y: usize = 2;
-    while (y < lines.lines.len) : (y += 2) {
+    while (y < rlr.lines.len) : (y += 2) {
         for (0..width) |x| {
-            const tx = beams.items[x];
-            if (tx > 0 and lines.lines[y][x] == '^') {
-                beams.items[x - 1] += tx;
-                beams.items[x + 1] += tx;
+            if (beams.items[x] > 0 and rlr.lines[y][x] == '^') {
+                beams.items[x - 1] += beams.items[x];
+                beams.items[x + 1] += beams.items[x];
                 beams.items[x] = 0;
                 splits += 1;
             }
