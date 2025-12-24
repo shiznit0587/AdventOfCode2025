f7e7f1a044e787fc900fa4f3f5ab8519d4450745
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2025-12-24 10:19:08 -0500

    Add zig-regex as a project dependency

--- build.zig
+++ build.zig
@@ -21,6 +21,9 @@ pub fn build(b: *std.Build) void {
     // target and optimize options) will be listed when running `zig build --help`
     // in this directory.
 
+    // -- Define dependencies here --
+    const regex = b.dependency("regex", .{ .target = target, .optimize = optimize }).module("regex");
+
     // This creates a module, which represents a collection of source files alongside
     // some compilation options, such as optimization mode and linked system libraries.
     // Zig modules are the preferred way of making Zig code available to consumers.
@@ -39,6 +42,10 @@ pub fn build(b: *std.Build) void {
         // Later on we'll use this module as the root module of a test executable
         // which requires us to specify a target.
         .target = target,
+        .imports = &.{
+            // -- Add dependencies here --
+            .{ .name = "regex", .module = regex },
+        },
     });
 
     // Here we define an executable. An executable needs to have a root module
